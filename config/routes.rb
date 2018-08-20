7e49fb5e410f3e58b97ced0f5ba18c982e5d1f20
Christopher Hutchins <blkangl@Christophers-MacBook-Pro-2.local>
2018-08-20 12:45:06 -0400

    unneeded code

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,5 @@ Rails.application.routes.draw do
   get 'wool/roving'
   get 'wool/yarn'
 
-  resources :products
-  resources :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
